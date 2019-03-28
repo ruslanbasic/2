79f9052370828bc4ba10eeef8f9dacefe63dbac3
Ruslan <rusbasv@gmail.com>
2019-03-28 17:58:57 +0200

    111

--- main.c
+++ main.c
@@ -19,99 +19,58 @@ int main(int argc, char *argv[])
     // Ініціалізація кольорового режиму і створення пари кольорів для фону і тексту
     start_color();
 
-    init_pair(COLOR_BLUE_YELLOW, COLOR_YELLOW, COLOR_BLUE);
+    init_pair(COLOR_BLUE_YELLOW, COLOR_BLACK, COLOR_WHITE);
 
     // Початок відображення в заданому кольоровому режимі
     attron(COLOR_PAIR(COLOR_BLUE_YELLOW));
 
     //COLOR_YELLOW фактично є коричневим. Для того щоб отримати жовтий, використовують COLOR_YELLOW 
     //в поєднанні з атрибутом A_BOLD.
-    attron(A_BOLD);
+//    attron(A_BOLD);
+//	erase();
+//	refresh();
 
+bkgdset (COLOR_PAIR(COLOR_BLUE_YELLOW));
+//clear();
+erase();
+refresh();
     
-    
-	printf ("Bm=\t");
+move (10, 10);
+
+//getyx(10,10);
+
+
+	curs_set(2);
+	printw ("Bm=\t");
     refresh();
-	scanf ("%f", &Bm);
+	scanw ("%f", &Bm);
 	
-	printf ("g=\t");
+	printw ("g=\t");
     refresh();
-	scanf ("%f", &g);
-	
+	scanw ("%f", &g);
+	curs_set(0);
 	Hg = Bm /mo;
-	printf ("Hg=\t%8.4f\n", Hg);
+	printw ("Hg=\t%8.4f\n", Hg);
 	refresh();
-	printf ("Le=\t");
-	scanf ("%f", &Le);
+	printw ("Le=\t");
+	scanw ("%f", &Le);
 	
-	printf ("Hm=\t");
-	scanf ("%f", &Hm);
+	printw ("Hm=\t");
+	scanw ("%f", &Hm);
 
 
 	IW = Hg * g * 1e-3 + Hm * 1e-3 * Le;
-	printf ("IW=\t%3.1f\n", IW);
+	printw ("IW=\t%3.1f\n", IW);
 	
 	
-	printf ("U1min=\t");
-	scanf ("%f", &U1min);
-
-	printf ("U_2=\t");
-	scanf ("%f", &U2);
-	
-	Kt = U1min / (U2 * 2);
-	printf ("Kt=\t%2.2f\n", Kt);
-	
-	printf ("I_2=\t");
-	scanf ("%f", &I2);
-
-	I2ef = I2 * sqrt(0.5);
-	printf ("I2ef=\t%2.2f\n", I2ef);
-	
-	I1ef = I2ef / Kt;
-	printf ("I1ef=\t%2.2f\n", I1ef);
-	
-	I1= I2/Kt;
-	printf ("I1=\t%2.2f\n", I1);
-	
-	printf ("f=\t");
-	scanf ("%f", &f);
-	
-	printf ("J=\t");
-	scanf ("%f", &J);
-
-	printf ("ko=\t");
-	scanf ("%f", &ko);
-	
-	Pu = U2 * 2 * I2ef;
-	printf ("Pu=\t%3.1f\n", Pu);
-	
-	ScSo = (100 * Pu)/(f * Bm * ko * J);
-	printf ("ScSo=\t%3.3f\n", ScSo);
-	
-	
-	printf ("Sc=\t");
-	scanf ("%f", &Sc);
-
-	printf ("So=\t");
-	scanf ("%f", &So);
-	
-	if ((Sc * So) > ScSo) printf ("ScSo=\t%3.3f\t\tГодится\n", Sc * So); 
-		else  printf ("ScSo=\t%3.3f\t\tНЕ годится\n", Sc * So);
-	
-	ew = 2e-4 * f * Bm * Sc; 
-	printf ("ew=\t%3.1f\n", ew);
-	
-	w1= U1min / ew;
-	w2= w1 / Kt;
-	printf ("w1=\t%4.1f\nw2=\t%4.1f\n", w1, w2);
-    
-    printw("kekekekekekedsdfsdvsdfvsd!\n");
+	    
+    printw("\n\n\nThe End!\n");
 
     attroff(A_BOLD);
 
     // Вивести текст без налаштування кольору
     attroff(COLOR_PAIR(1));
-    printw("sdfgsdfsdfvgsfrierpiehjrij\n");
+    printw("OUT\n");
 
     // Оновлення екрану
     refresh();
